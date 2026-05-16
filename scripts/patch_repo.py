3b40751fb2450edd5c01d5f9acaf68297cdcc693
BanyAdm <sohaibextra69@gmail.com>
2026-05-16 13:51:23 +0300

    change package name to avoid install conflict

--- scripts/patch_repo.py
+++ scripts/patch_repo.py
@@ -24,3 +24,8 @@ with open('android/src/main/AndroidManifest.xml', 'w') as f:
     f.write(c)
 
 print("Manifest patched OK")
+
+# Change package name to avoid conflict with official Tailscale
+import subprocess
+subprocess.run(['sed', '-i', "s/namespace 'com.tailscale.ipn'/namespace 'com.banyadm.tailscale'/", 'android/build.gradle'], check=True)
+print("Package name patched OK")
