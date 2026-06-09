643471270d924a07e149700c2eae056ba752286c
Bany Adm <160159582+BanyAdm@users.noreply.github.com>
2026-06-09 18:35:43 +0300

    Correct URL for tailscale installation
    Fix URL in curl command to use 'github.com' instead of 'github'.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 set -e
+clear
 echo "Installing termux-tailscale.."
-curl -L -o $PREFIX/bin/tailscale "https://raw.githubusercontent/banyadm/termux-tailscale/main/scripts/tailscale"
+curl -L -o $PREFIX/bin/tailscale "https://raw.githubusercontent.com/banyadm/termux-tailscale/main/scripts/tailscale"
 chmod +x $PREFIX/bin/tailscale
 echo "termux-tailscale is done installing! to use it run 'tailscale'"
