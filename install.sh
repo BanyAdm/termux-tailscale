7cb3c76f138f272fd685d074cca5117388d52065
Bany Adm <160159582+BanyAdm@users.noreply.github.com>
2026-06-09 18:26:42 +0300

    Enhance install script for termux-tailscale
    Added error handling and installation messages.

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+set -e
+echo "Installing termux-tailscale.."
 curl -L -o $PREFIX/bin/tailscale "https://raw.githubusercontent/banyadm/termux-tailscale/main/scripts/tailscale"
 chmod +x $PREFIX/bin/tailscale
+echo "termux-tailscale is done installing! to use it run 'tailscale'"
